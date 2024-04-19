504d921aeb8094a8b47733451a12dc98577f593a
tornierm <80321344+Tornierm@users.noreply.github.com>
2024-04-19 14:37:39 +0200

    updated app

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Sitdown you fuckers.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +8,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/S.png')}/>
+      <Text>Welcome to Sitdown you fuckers.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
