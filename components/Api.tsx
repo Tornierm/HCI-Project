f97360559281e479c2a6d5b3a514be71e4349d13
MttDpt <m.matteodelprato@gmail.com>
2024-04-30 13:07:07 +0200

    current state

--- components/Api.tsx
+++ components/Api.tsx
@@ -34,7 +34,37 @@ const CoffeeShops: ICafe[] = [
     /*
         add more cafes here
     */
-]
+        {
+            name: "Sukis cafè",
+            restrictions: [
+                Restrictions.Halal,
+                Restrictions.Vegan
+            ],
+            offers: [
+                {
+                    name: "super offer",
+                    description: "This describes everything that is offered in this offer.",
+                    price: 20,
+                }
+            ],
+            reviews: [
+                {
+                    userId: 0,
+                    comment: undefined,
+                    imageSrc: undefined
+                }
+            ],
+            features: [
+                Features.Laptop,
+            ],
+            location: {
+                left: 300,
+                top: 300,
+            },
+            rating: Rating.worst,
+            price: Price.cheap,
+        },
+]   
 
 const Friends: IUser[] = [
     {
